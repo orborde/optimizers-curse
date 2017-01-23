c22f70e09ccc687049c68b35889d37a64fea7d9e
William Ehlhardt <williamehlhardt@gmail.com>
2017-01-22 17:37:41 -0800

    Start hacking around.

---
+++ expected-value.py
@@ -0,0 +1,19 @@
+# Find the expected dollar value of each possible label value.
+
+import collections
+import itertools
+
+# Maps "number on the box" to "actual dollars in the box"
+outcomes = collections.defaultdict(list)
+
+for dollars,fiction in itertools.product(range(1,10+1), repeat=2):
+    label = dollars + fiction
+    outcomes[label].append(dollars)
+
+def mean(arr):
+    return sum(arr) / float(len(arr))
+
+for label in sorted(outcomes.keys()):
+    values = outcomes[label]
+    print label, mean(values)
+
