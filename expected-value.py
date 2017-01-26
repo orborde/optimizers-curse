81eb7960b438a551061c744c4fff24c186648a88
William Ehlhardt <williamehlhardt@gmail.com>
2017-01-25 18:32:11 -0800

    Change expected-value table to match the problem statement.
    Of course, this immediately reveals that even an unbiased error offset doesn't result in the
    label being an unbiased estimator of what's in the box. Oops.

--- expected-value.py
+++ expected-value.py
@@ -6,7 +6,7 @@ import itertools
 # Maps "number on the box" to "actual dollars in the box"
 outcomes = collections.defaultdict(list)
 
-for dollars,fiction in itertools.product(range(1,10+1), repeat=2):
+for dollars,fiction in itertools.product(range(1,10+1), range(-4, 5, 1)):
     label = dollars + fiction
     outcomes[label].append(dollars)
 
