93d16f2e4ea4b1a893d3dedd4fdf1827c383f55e
William Ehlhardt <williamehlhardt@gmail.com>
2017-01-25 19:14:36 -0800

    Rewrite expected-value to compute the actual scenario. Results are not promising.

--- expected-value.py
+++ expected-value.py
@@ -3,17 +3,34 @@
 import collections
 import itertools
 
-# Maps "number on the box" to "actual dollars in the box"
-outcomes = collections.defaultdict(list)
+from box_generator import LABELS, BOXES
 
-for dollars,fiction in itertools.product(range(1,10+1), range(-4, 5, 1)):
-    label = dollars + fiction
-    outcomes[label].append(dollars)
-
+# No builtin for this in Py2? Really? :(
 def mean(arr):
     return sum(arr) / float(len(arr))
 
-for label in sorted(outcomes.keys()):
-    values = outcomes[label]
-    print label, mean(values)
+def compute_ev_table(k):
+    outcomes = collections.defaultdict(list)
+    for boxes in itertools.product(BOXES, repeat=k):
+        selected_box = max(boxes, key=lambda box: box[0])
+        label,actual = selected_box
+        outcomes[label].append(actual)
 
+    ev = {}
+    for label in outcomes:
+        ev[label] = mean(outcomes[label])
+    return ev
+
+print len(BOXES), 'boxes'
+results_table = {}
+ks = range(1, 3+1)
+for k in ks:
+    print 'COMPUTING', k
+    results_table[k] = compute_ev_table(k)
+
+print '==== RESULTS ===='
+def print_row(label, values):
+    print ' '.join('{:<10}'.format(v) for v in ([label] + values))
+print_row('label', ['k={}'.format(k) for k in ks])
+for label in LABELS:
+    print_row(label, [results_table[k][label] for k in ks])
